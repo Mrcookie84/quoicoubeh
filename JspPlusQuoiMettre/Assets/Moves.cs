c4ad77bef1af727b812102ad13b0ff2793396371
Mrcookie <hautcoeur.corentin@gmail.com>
2024-09-30 12:43:43 +0200

    4

--- JspPlusQuoiMettre/Assets/Moves.cs
+++ JspPlusQuoiMettre/Assets/Moves.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private KeyCode _keyCodeZ = KeyCode.W;
+    [SerializeField] private KeyCode _keyCodeQ = KeyCode.A;
+    [SerializeField] private KeyCode _keyCodeS = KeyCode.S;
+    [SerializeField] private KeyCode _keyCodeD = KeyCode.D;
 
     void Update()
     {
-        
+        if (Input.GetKey(_keyCodeZ))
+        {
+            transform.position += Vector3.forward;
+        }
+
+        if (Input.GetKey(_keyCodeQ))
+        {
+            transform.position += Vector3.left;
+        }
+
+        if (Input.GetKey(_keyCodeS))
+        {
+            transform.position += Vector3.back;
+        }
+
+        if (Input.GetKey(_keyCodeD))
+        {
+            transform.position += Vector3.right;
+        }
     }
 }
